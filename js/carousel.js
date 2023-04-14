51a2415b0ce4ccfdb86ba4b15cc0970b53c8687e
Anton <101654796+Sh3ddy@users.noreply.github.com>
2023-04-14 11:36:29 +0300

    fix carousel
    + title in footer

--- js/carousel.js
+++ js/carousel.js
@@ -45,39 +45,39 @@ const autoSlide = () => {
     positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
 };
 
-const dragStart = (e) => {
-  isDragStart = true;
-  prevPageX = e.pageX || e.touches[0].pageX;
-  prevScrollLeft = carousel.scrollLeft;
-};
+// const dragStart = (e) => {
+//   isDragStart = true;
+//   prevPageX = e.pageX || e.touches[0].pageX;
+//   prevScrollLeft = carousel.scrollLeft;
+// };
 
-const dragging = (e) => {
-  if (!isDragStart) return;
-  e.preventDefault();
-  isDragging = true;
-  carousel.classList.add("dragging");
-  positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
-  carousel.scrollLeft = prevScrollLeft - positionDiff;
-  showHideIcons();
-};
+// const dragging = (e) => {
+//   if (!isDragStart) return;
+//   e.preventDefault();
+//   isDragging = true;
+//   carousel.classList.add("dragging");
+//   positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+//   carousel.scrollLeft = prevScrollLeft - positionDiff;
+//   showHideIcons();
+// };
 
-const dragStop = () => {
-  isDragStart = false;
-  carousel.classList.remove("dragging");
+// const dragStop = () => {
+//   isDragStart = false;
+//   carousel.classList.remove("dragging");
 
-  if (!isDragging) return;
-  isDragging = false;
-  autoSlide();
-};
+//   if (!isDragging) return;
+//   isDragging = false;
+//   autoSlide();
+// };
 
-carousel.addEventListener("mousedown", dragStart);
-carousel.addEventListener("touchstart", dragStart);
+// carousel.addEventListener("mousedown", dragStart);
+// carousel.addEventListener("touchstart", dragStart);
 
-document.addEventListener("mousemove", dragging);
-carousel.addEventListener("touchmove", dragging);
+// document.addEventListener("mousemove", dragging);
+// carousel.addEventListener("touchmove", dragging);
 
-document.addEventListener("mouseup", dragStop);
-carousel.addEventListener("touchend", dragStop);
+// document.addEventListener("mouseup", dragStop);
+// carousel.addEventListener("touchend", dragStop);
 
 // ДЛЯ ЯКОРЕЙ
 
